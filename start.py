8f1f4a31b2e8220d081e6d239fed73774f69716e
tamco61 <42232243+tamco61@users.noreply.github.com>
2022-07-05 05:29:51 -0700

    rework start and main
    чисто косметические изменения
    +62 строка todo

--- start.py
+++ start.py
@@ -7,7 +7,8 @@ import time
 
 PATH_TO_PYTHON = "C:/Users/USER/pythonProject/Project/new/Scripts/python.exe"
 
-def cropwithoverlap_imgs(path_in, path_out, n_w, n_h, overlap):
+
+def cropwithoverlap_imgs(path_in, path_out, n_w, n_h, overlap, height, width):
     if path_in[-1] != "/":
         path_in += "/"
     if path_out[-1] != "/":
@@ -18,8 +19,8 @@ def cropwithoverlap_imgs(path_in, path_out, n_w, n_h, overlap):
     if not os.path.exists(path_out):
         os.makedirs(path_out)
 
-    w = 1920 // n_w
-    h = 1080 // n_h
+    w = width // n_w
+    h = height // n_h
 
     count = 1
     for name in filenames:
@@ -83,36 +84,33 @@ def col():
         cv2.imwrite("./examples/collage/" + name, Vertical_attachment)
 
 
-def vid(pathout):
+def vid(pathout, height, width, name):
     path = 'examples/collage'
     lst = os.listdir(path)
     lst = sorted(lst)
     listframe = [path + '/' + name for name in lst]
     frames = []
-    print(lst)
     for fr in listframe:
         image = cv2.imread(fr)
         image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         frames.append(image)
-    print('Saving videos...')
     res_dir = 'results'  # Название папки для сохранения
     res_name = '_results.mp4'  # Название готового файла
     # old_name = args.video.split('/')[-1]  # Первоначальное название видео
-    video_length = 49
+    video_length = len(listframe)
     comp_frames = [np.array(f).astype(np.uint8) for f in frames]
     #save_name = old_name.replace('.mp4', res_name) if args.use_mp4 else old_name + res_name
     if not os.path.exists(res_dir):
         os.makedirs(res_dir)
-    save_path = os.path.join(pathout, 'anime.mp4')
-    writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), 24, (1920, 1080))
+    save_path = os.path.join(pathout, name + res_name)
+    writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), 24, (height, width))
     for f in range(video_length):
         comp = comp_frames[f].astype(np.uint8)
         writer.write(cv2.cvtColor(comp, cv2.COLOR_BGR2RGB))
     writer.release()
-    print(f'Finish test! The result video is saved in: {save_path}.')
 
 
-def cycle(fileinput, fileoutput, maskinput, step, neighbor):
+def cycle(fileinput, fileoutput, maskinput, step, neighbor, height, width, name):
     start_time = time.time()
 
     if not os.path.exists("examples/v"):
@@ -121,13 +119,13 @@ def cycle(fileinput, fileoutput, maskinput, step, neighbor):
     if not os.path.exists("examples/v_mask"):
         os.makedirs("examples/v_mask")
 
-    cropwithoverlap_imgs(fileinput, "examples/v", 2, 2, 40)
-    cropwithoverlap_imgs(maskinput, "examples/v_mask", 2, 2, 40)
+    cropwithoverlap_imgs(fileinput, "examples/v", 2, 2, 40, height, width)
+    cropwithoverlap_imgs(maskinput, "examples/v_mask", 2, 2, height, width)
 
-    os.system(f'{PATH_TO_PYTHON} test1.py -n 1 --step {int(step)} --neighbor_stride {int(neighbor)} --model e2fgvi_hq --video ./examples/v/1 --mask ./examples/v_mask/1  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
-    os.system(f'{PATH_TO_PYTHON} test1.py -n 2 --step {int(step)} --neighbor_stride {int(neighbor)} --model e2fgvi_hq --video ./examples/v/2 --mask ./examples/v_mask/2  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
-    os.system(f'{PATH_TO_PYTHON} test1.py -n 3 --step {int(step)} --neighbor_stride {int(neighbor)} --model e2fgvi_hq --video ./examples/v/3 --mask ./examples/v_mask/3  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
-    os.system(f'{PATH_TO_PYTHON} test1.py -n 4 --step {int(step)} --neighbor_stride {int(neighbor)} --model e2fgvi_hq --video ./examples/v/4 --mask ./examples/v_mask/4  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
+    os.system(f'{PATH_TO_PYTHON} test.py -n 1 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/1 --mask ./examples/v_mask/1  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
+    os.system(f'{PATH_TO_PYTHON} test.py -n 2 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/2 --mask ./examples/v_mask/2  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
+    os.system(f'{PATH_TO_PYTHON} test.py -n 3 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/3 --mask ./examples/v_mask/3  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
+    os.system(f'{PATH_TO_PYTHON} test.py -n 4 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/4 --mask ./examples/v_mask/4  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
 
     video = "examples/v"
     mask = "examples/v_mask"
@@ -136,7 +134,7 @@ def cycle(fileinput, fileoutput, maskinput, step, neighbor):
 
     col()
 
-    vid(fileoutput)
+    vid(fileoutput, height, width, name)
 
 
     print("--- %s seconds ---" % (time.time() - start_time))
