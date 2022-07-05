44abee075e57aa763efe64f29fe98445d51a278b
tamco61 <42232243+tamco61@users.noreply.github.com>
2022-07-05 08:46:11 -0700

    rework test.py and little bit changed start.py
    убрал парсер из test.py, сделав из скрипта модуль который куда хочешь импортируешь
    импортировал test.py в start.py

--- start.py
+++ start.py
@@ -3,9 +3,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import time
-
-
-PATH_TO_PYTHON = "C:/Users/USER/pythonProject/Project/new/Scripts/python.exe"
+import test
 
 
 def cropwithoverlap_imgs(path_in, path_out, n_w, n_h, overlap, height, width):
@@ -75,8 +73,6 @@ def col():
         img3 = cv2.imread("./examples/result/3/" + name)
         img4 = cv2.imread("./examples/result/4/" + name)
 
-
-
         Horizontal1=np.hstack([img1,img2])
         Horizontal2=np.hstack([img3,img4])
         Vertical_attachment=np.vstack([Horizontal1,Horizontal2])
@@ -84,16 +80,18 @@ def col():
         cv2.imwrite("./examples/collage/" + name, Vertical_attachment)
 
 
-def vid(pathout, height, width, name):
+def vid(pathout, height, width):
     path = 'examples/collage'
     lst = os.listdir(path)
     lst = sorted(lst)
     listframe = [path + '/' + name for name in lst]
     frames = []
+    print(lst)
     for fr in listframe:
         image = cv2.imread(fr)
         image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         frames.append(image)
+    print('Saving videos...')
     res_dir = 'results'  # Название папки для сохранения
     res_name = '_results.mp4'  # Название готового файла
     # old_name = args.video.split('/')[-1]  # Первоначальное название видео
@@ -102,39 +100,37 @@ def vid(pathout, height, width, name):
     #save_name = old_name.replace('.mp4', res_name) if args.use_mp4 else old_name + res_name
     if not os.path.exists(res_dir):
         os.makedirs(res_dir)
-    save_path = os.path.join(pathout, name + res_name)
-    writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), 24, (height, width))
+    save_path = os.path.join(pathout, 'anime.mp4')
+    writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), 24, (width, height))
     for f in range(video_length):
         comp = comp_frames[f].astype(np.uint8)
         writer.write(cv2.cvtColor(comp, cv2.COLOR_BGR2RGB))
     writer.release()
+    print(f'Finish test! The result video is saved in: {save_path}.')
 
 
-def cycle(fileinput, fileoutput, maskinput, step, neighbor, height, width, name):
+def cycle(fileinput, fileoutput, maskinput, step, neighbor, height, width):
     start_time = time.time()
-
-    if not os.path.exists("examples/v"):
-        os.makedirs("examples/v")
-
-    if not os.path.exists("examples/v_mask"):
-        os.makedirs("examples/v_mask")
-
-    cropwithoverlap_imgs(fileinput, "examples/v", 2, 2, 40, height, width)
-    cropwithoverlap_imgs(maskinput, "examples/v_mask", 2, 2, height, width)
-
-    os.system(f'{PATH_TO_PYTHON} test.py -n 1 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/1 --mask ./examples/v_mask/1  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
-    os.system(f'{PATH_TO_PYTHON} test.py -n 2 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/2 --mask ./examples/v_mask/2  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
-    os.system(f'{PATH_TO_PYTHON} test.py -n 3 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/3 --mask ./examples/v_mask/3  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
-    os.system(f'{PATH_TO_PYTHON} test.py -n 4 --step {step} --neighbor_stride {neighbor} --model e2fgvi_hq --video ./examples/v/4 --mask ./examples/v_mask/4  --ckpt release_model/E2FGVI-HQ-CVPR22.pth --width 1000 --height 580')
 
     video = "examples/v"
     mask = "examples/v_mask"
+
+    if not os.path.exists(video):
+        os.makedirs(mask)
+
+    if not os.path.exists(video):
+        os.makedirs(mask)
+
+    cropwithoverlap_imgs(fileinput, video, 2, 2, 40, height, width)
+    cropwithoverlap_imgs(maskinput, mask, 2, 2, 40, height, width)
+
+    test.main_worker(video, mask, "release_model/E2FGVI-HQ-CVPR22.pth", 4, "e2fgvi_hq", neighbor, step, 1000, 580)
 
     newcrop()
 
     col()
 
-    vid(fileoutput, height, width, name)
+    vid(fileoutput, height, width)
 
 
     print("--- %s seconds ---" % (time.time() - start_time))
